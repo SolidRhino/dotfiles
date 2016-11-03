ec778a5b6d73fc5fff4d12b01f2adf65191a802d
SolidRhino <git@ivotrompert.nl>
2016-11-03 15:06:23 +0100

    Reorder file

--- node/install.sh
+++ node/install.sh
@@ -3,12 +3,11 @@ then
   if test ! $(which yarn)
   then
     npm install --global yarn
-  else
-    yarn self-update
   fi
 
   if test $(which yarn)
   then
+    yarn self-update
     yarn global add spoof
     yarn global add react-native-cli
   fi
