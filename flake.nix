11376a41ee79b6c2b84f77c22d8b1359d13a45c5
SolidRhino <SolidRhino@users.noreply.github.com>
2024-11-27 10:56:08 +0100

    Add helix cachix to the extra-substituters

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://helix.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
     ];
   };
 
